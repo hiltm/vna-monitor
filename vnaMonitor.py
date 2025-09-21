78a0580c0c9fba977f9c746a0060ff5b60d83003
Michael Hilt <michael.hilt4@gmail.com>
2025-09-21 18:44:02 -0400

    Switching to pynanovna

--- vnaMonitor.py
+++ vnaMonitor.py
@@ -1,8 +1,19 @@
-import pyvisa
+import pynanovna
+import datetime
 
-vna = 'TCPIP0::localhost::hislip0::INSTR'
-rm = pyvisa.ResourceManager()
-inst = rm.open_resource(vna)
-inst.timeout = 20000
- 
-print(inst.query("*IDN?"))
+vna = pynanovna.VNA()
+print(vna.info())
+filename = '/home/chiptamper/chiptamper/nanovna_' + str(datetime.datetime.now()) + '.csv'
+
+vna.stream_to_csv(filename, nr_sweeps=100, skip_start=5, sweepdivider='sweepnumber: ')
+
+#vna.set_sweep(1.0e9, 1.4e9, 101)
+#s11, s21, frequencies = vna.sweep()
+#print(s11)
+#print(s21)
+#print(frequencies)
+
+# continuous sweep
+#for s11, s21, frequencies in vna.stream():
+#    print(s11, s21, frequencies)
+#    # Do what I want with each sweep here
