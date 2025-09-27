71ec2cbd8528d53c875d0485d6aa76637e105946
Michael Hilt <michael.hilt4@gmail.com>
2025-09-26 20:26:44 -0400

    Calling a calibration file into the monitor program

--- vnaMonitor.py
+++ vnaMonitor.py
@@ -2,18 +2,21 @@ import pynanovna
 import datetime
 
 vna = pynanovna.VNA()
+
+filename = '/home/chiptamper/chiptamper/logs/nanovna_' + str(datetime.datetime.now()) + '.csv'
+calibrationFile = '/home/chiptamper/chiptamper/Calibration_1758496936.2956493.cal'
+
+vna.load_calibration(calibrationFile)
 print(vna.info())
-filename = '/home/chiptamper/chiptamper/nanovna_' + str(datetime.datetime.now()) + '.csv'
+#vna.stream_to_csv(filename, nr_sweeps=100, skip_start=5, sweepdivider='sweepnumber: ')
 
-vna.stream_to_csv(filename, nr_sweeps=100, skip_start=5, sweepdivider='sweepnumber: ')
-
-#vna.set_sweep(1.0e9, 1.4e9, 101)
+vna.set_sweep(120000, 140000000, 101)
 #s11, s21, frequencies = vna.sweep()
 #print(s11)
 #print(s21)
 #print(frequencies)
 
 # continuous sweep
-#for s11, s21, frequencies in vna.stream():
-#    print(s11, s21, frequencies)
+for s11, s21, frequencies in vna.stream():
+    print(s11, s21, frequencies)
 #    # Do what I want with each sweep here
