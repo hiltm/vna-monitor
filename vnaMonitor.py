0525a73de9a4113ccd43b880caa50c532524de82
Michael Hilt <michael.hilt4@gmail.com>
2025-10-01 02:15:35 +0100

    Resolved the too many retries IO error by reducing the amount of points captured per sweep, additional handling for the sweep to catch errors

--- vnaMonitor.py
+++ vnaMonitor.py
@@ -13,7 +13,7 @@ print(vna)
 plotting_enabled = True
 start_freq = 1e6
 stop_freq = 1.5e6
-points = 101
+points = 10 #101
 
 plt.ion()
 fig, ax = plt.subplots()
@@ -27,14 +27,24 @@ def set_global_filename(filename_concat):
 def get_global_filename():
     return filename_glb
 
+def safe_sweep(vna, retries=3, delay=2):
+    for attempt in range(retries):
+        try:
+            s11, s21, freqs = vna.sweep()
+            s11 = np.array(s11)
+            s21 = np.array(s21)
+            freqs = np.array(freqs)
+            return np.array(s11), np.array(s21), np.array(freqs)
+        except Exception as e:
+            print(f"Error on sweep attempt {attempt+1}: {e}")
+            time.sleep(delay)
+    raise RuntimeError("Failed to complete sweep after multiple retires")
+
 def capture_data(filename, start_freq, stop_freq, points):
 
     vna.set_sweep(start_freq, stop_freq, points)
 
-    s11, s21, freqs = vna.sweep()
-    s11 = np.array(s11)
-    s21 = np.array(s21)
-    freqs = np.array(freqs)
+    s11, s21, freqs = safe_sweep(vna, 3, 2)
 
     save_2port = True
     if save_2port:
@@ -74,7 +84,7 @@ try:
         filename = f"/home/chiptamper/chiptamper/testdata/measurement_{timestamp}_"
 
         print("Taking measurement...")
-        capture_data(filename=filename, start_freq=1e6, stop_freq=1.5e6, points=101)
+        capture_data(filename=filename, start_freq=1e6, stop_freq=1.5e6, points=10)#101)
 
         ax.clear()
 #        smith_chart = rf.
@@ -82,9 +92,9 @@ try:
         ntwk.plot_s_smith(ax=ax)
         ax.set_title(f"Smith Chart @ {get_global_filename()}")
         plt.draw()
-        plt.pause(5)
+        plt.pause(1)
 
-        time.sleep(10)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("stopped")
