98aef93534726e7908b71248cc3cee8e1234a7b2
Michael Hilt <michael.hilt4@gmail.com>
2025-10-01 01:59:20 +0100

    Take a sweep using pynanovna, then save that data and recall it using scikit-rf, plot using matplotlib. Caution: this code is hitting IOError too many retries

--- vnaMonitor.py
+++ vnaMonitor.py
@@ -1,24 +1,95 @@
+import numpy as np
 import pynanovna
-import datetime
+from datetime import datetime
 import skrf as rf
+#from skrf.vi.vna.nanovna import NanoVNAv2
+import matplotlib.pyplot as plt
+import time
 
 vna = pynanovna.VNA()
+#vna = NanoVNAv2("ASRL/dev/ttyACM0::INSTR")
+print(vna)
 
-filename = '/home/chiptamper/chiptamper/logs/nanovna_' + str(datetime.datetime.now()) + '.csv'
-calibrationFile = '/home/chiptamper/chiptamper/Calibration_1758496936.2956493.cal'
+plotting_enabled = True
+start_freq = 1e6
+stop_freq = 1.5e6
+points = 101
 
-vna.load_calibration(calibrationFile)
-print(vna.info())
+plt.ion()
+fig, ax = plt.subplots()
+smith_chart = rf.setup_plotting()
 
-for data in vna.stream():
-    frequencies = data[0]
-    s21 = data[1]
-    s22 = data[2]
+def set_global_filename(filename_concat):
+    global filename_glb 
+    filename_glb = filename_concat
+    return None
 
-#frequencies, s21, s22 = vna.stream()
+def get_global_filename():
+    return filename_glb
 
-    print(f"{'Index':>5} | {'Frequency (Hz)':>15} | {'S21':>12} | {'S22':>12}")
-    print("-" * 55)
+def capture_data(filename, start_freq, stop_freq, points):
 
-    for i, (f, val21, val22) in enumerate(zip(frequencies, s21, s22)):
-        print(f"{i:5d} | {f:15.2f} | {val21:12.6f} | {val22:12.6f}")
+    vna.set_sweep(start_freq, stop_freq, points)
+
+    s11, s21, freqs = vna.sweep()
+    s11 = np.array(s11)
+    s21 = np.array(s21)
+    freqs = np.array(freqs)
+
+    save_2port = True
+    if save_2port:
+        N = len(freqs)
+        s = np.zeros((N, 2, 2), dtype=complex)
+        s[:, 0, 0] = s11
+        s[:, 0, 1] = s21
+
+        freq_obj = rf.Frequency.from_f(freqs, unit='Hz')
+        ntwk = rf.Network(frequency=freq_obj, s=s, name="NanoVNA 2-port")
+        filename_concat = filename + "testdata.s2p"
+        set_global_filename(filename_concat)
+        ntwk.write_touchstone(filename_concat)
+        print(f"Saved 2-port Touchstone as {filename_concat}")
+        #if(plotting_enabled):
+        #    ntwk.plot_s_smith()
+        #    plt.show()
+    else:
+        N = len(freqs)
+        s = s11.reshape(N, 1, 1)
+
+        freq_obj = rf.Frequency.from_f(freqs, unit='Hz')
+        ntwk = rf.Network(frequency=freq_obj, s=s, name="NanoVNA 1-port")
+        filename_concat = filename + "testdata.s1p"
+        set_global_filename(filename_concat)
+        ntwk.write_touchstone(filename_concat)
+        print(f"Saved 2-port Touchstone as {filename_concat}")
+        #if(plotting_enabled):
+        #    ntwk.plot_s_smith()
+        #    plt.show()
+
+#if __name__ == "__main__":
+try:
+    while(True):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        print(timestamp)
+        filename = f"/home/chiptamper/chiptamper/testdata/measurement_{timestamp}_"
+
+        print("Taking measurement...")
+        capture_data(filename=filename, start_freq=1e6, stop_freq=1.5e6, points=101)
+
+        ax.clear()
+#        smith_chart = rf.
+        ntwk = rf.Network(get_global_filename())
+        ntwk.plot_s_smith(ax=ax)
+        ax.set_title(f"Smith Chart @ {get_global_filename()}")
+        plt.draw()
+        plt.pause(5)
+
+        time.sleep(10)
+
+except KeyboardInterrupt:
+    print("stopped")
+
+#finally:
+#    vna.close()
+#    plt.ioff()
+#    plt.show()
