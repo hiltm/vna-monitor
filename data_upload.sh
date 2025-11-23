3dbae009aff66e8a3f8c92d6ecf0e777b7dd3236
Michael Hilt <michael.hilt4@gmail.com>
2025-11-23 02:05:00 +0000

    Changes to getting the continuous mode working

--- data_upload.sh
+++ data_upload.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#get current date for folder name to check
-date=$(date '+%Y%m%d')
-
-#monitor this directory
-WATCH_DIR="/home/chiptamper/chiptamper/data/${date}"
-#upload to this directory
-DEST_DIR="/home/chiptamper/ecresearch/data/${date}"
-
-if [ ! -d "$DEST_DIR" ]; then
-   sudo mkdir -p "$DEST_DIR"
-   echo "made directory ${DEST_DIR}"
-else
-   echo "directory ${DEST_DIR} already exists"
-fi
+create_directory_if_needed() {
+   local dir="$DEST_DIR"
+   if [ ! -d "$dir" ]; then
+      sudo mkdir -p "$dir"
+      echo "made directory ${dir}"
+   else
+      echo "directory ${dir} already exists"
+   fi
+}
 
 #time to wait after the last file writing before uploading in seconds
 INACTIVITY_TIME=300
@@ -21,20 +16,13 @@ INACTIVITY_TIME=300
 #tmp file to track the last modifition time of files
 LAST_MOD_TIME="/tmp/last_mod_time.txt"
 
-if [ ! -d "$LAST_MOD_TIME" ]; then
-   sudo touch "$LAST_MOD_TIME"
-   echo "made file ${LAST_MOD_TIME}"
-else
-   echo "file ${LAST_MOD_TIME} already exists"
-fi
-
 upload_data() {
    echo "Uploading data"
    scp -p $WATCH_DIR/* $DEST_DIR
 }
 
 monitor_dir() {
-   inotifywait -m -r -e modify,create,move,delete "$WATCH_DIR" | while read -r path action file; do
+   inotifywait -t $INACTIVITY_TIME -m -r -e modify,create,move,delete "$WATCH_DIR" | while read -r path action file; do
       CURRENT_TIME=$(date +%s)
 
       #get the time of the last modification from the tmp file
@@ -43,6 +31,11 @@ monitor_dir() {
       else
          LAST_TIME=0
       fi
+
+#      TMP=$((CURRENT_TIME-LAST_TIME))
+#      echo $TMP
+
+#      echo $INACTIVITY_TIME
 
       #backup data if enough time has passed since the last upload
       if (( CURRENT_TIME - LAST_TIME >= INACTIVITY_TIME )); then
@@ -56,4 +49,26 @@ monitor_dir() {
 
 #start the monitoring process
 echo "Starting process"
+
+#track the current date to determine today's directories
+date=$(date '+%Y%m%d')
+
+#monitor this directory
+WATCH_DIR="/home/chiptamper/chiptamper/data/${date}"
+
+#upload to this directory
+DEST_DIR="/home/chiptamper/ecresearch/data/${date}"
+
+#ensure the directory exists
+create_directory_if_needed "$DEST_DIR"
+
+#ensure the last modification time file exists
+if [ ! -f "$LAST_MOD_TIME" ]; then
+   sudo touch "$LAST_MOD_TIME"
+   echo "made file ${LAST_MOD_TIME}"
+else
+   echo "file ${LAST_MOD_TIME} already exists"
+fi
+
+#monitor the directory
 monitor_dir
